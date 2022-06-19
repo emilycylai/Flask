54699d7d7cd808ac3be0aeaf68d0d33e9b808db1
Emily <lai810717@gmail.com>
2022-06-19 12:17:08 +0800

    stocks

--- main.py
+++ main.py
@@ -10,14 +10,34 @@ def index():
     return "<h1>Hello World!</h1>"
 
 
+@app.route('/stock')
+def stock():
+    stocks = [
+        {'分類': '日經指數', '指數': '22,920.30'},
+        {'分類': '韓國綜合', '指數': '2,304.59'},
+        {'分類': '香港恆生', '指數': '25,083.71'},
+        {'分類': '上海綜合', '指數': '3,380.68'}
+    ]
+    for stock in stocks:
+        print(stock['分類'], stock['指數'])
+    return render_template('./stocks.html', stocks=stocks)
+
+
 @app.route('/sum/x=<x>&y=<y>')
 def get_sum(x, y):
     try:
         total = str(eval(x)+eval(y))
-        return f'{x}+{y} 總和為：{total}'
+        # return f'{x}+{y} 總和為：{total}'
     except Exception as e:
         print(e)
-        return '<h1 style="color:red">輸入錯誤！</h1>'
+        total = '輸入錯誤！'
+        # return '<h1 style="color:red">輸入錯誤！</h1>'
+    result = {
+        'x': x,
+        'y': y,
+        'total': total
+    }
+    return render_template('./index.html', result=result)
 
 
 @app.route('/today')
