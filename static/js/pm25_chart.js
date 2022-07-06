fe0d4759f0df39897a22cdafee6014c39b02dc87
Emily <lai810717@gmail.com>
2022-07-06 22:19:02 +0800

    0706_3

--- static/js/pm25_chart.js
+++ static/js/pm25_chart.js
@@ -13,8 +13,8 @@ function drawPM25(){
             dataType:"json",
             success:(data) => {
                 console.log(data);
-                //drawChart_1(data);
-                drawChart_2(data);
+                drawChart_1(data);
+                //drawChart_2(data);
                  
 
             },
@@ -45,20 +45,7 @@ function drawChart_2(data2){
             text: '特性示例：渐变色 阴影 点击缩放',
             subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
         },
-        toolbox:{
-            shou:true,
-            orient:'vertical',
-            left:'left',
-            top:'center',
-            feature:{
-                magictype:{ show: true, type: ['line','bar','tiled']},
-                restore:{show: true},
-                saveAsImage:{show: true}
-            }
-        },
-        tooltip:{
-            trigger: 'axis'
-        },
+        
         xAxis: {
             data: dataAxis,
             axisLabel: {
@@ -138,7 +125,20 @@ function drawChart_1(data){
         title: {
             text: "PM2.5 數據圖",
         },
-        tooltip: {},
+        toolbox:{
+            shou:true,
+            orient:'vertical',
+            left:'left',
+            top:'center',
+            feature:{
+                magictype:{ show: true, type: ['line','bar','tiled']},
+                restore:{show: true},
+                saveAsImage:{show: true}
+            }
+        },
+        tooltip:{
+            trigger: 'axis'
+        },
         legend: {
             data: ["站點"],
         },
