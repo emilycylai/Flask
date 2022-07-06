98b8b6354aa227694d3807d98b104d8b15fa8fd0
Emily <lai810717@gmail.com>
2022-07-06 21:21:46 +0800

    0706_1

--- static/js/pm25_chart.js
+++ static/js/pm25_chart.js
@@ -13,32 +13,9 @@ function drawPM25(){
             dataType:"json",
             success:(data) => {
                 console.log(data);
-                let myChart = echarts.init(mainEl);
-
-                // 指定图表的配置项和数据
-                let option = {
-                    title: {
-                        text: "PM2.5 數據圖",
-                    },
-                    tooltip: {},
-                    legend: {
-                        data: ["站點"],
-                    },
-                    xAxis: {
-                        data:data["stationName"],
-                    },
-                    yAxis: {},
-                    series: [
-                        {
-                            name: "PM2.5",
-                            type: "bar",
-                            data: data["result"],
-                        },
-                    ],
-                };
-
-                    // 使用刚指定的配置项和数据显示图表。
-                myChart.setOption(option);  
+                //drawChart_1(data);
+                drawChart_2(data);
+                 
 
             },
 
@@ -49,5 +26,122 @@ function drawPM25(){
     )
 }
 
+function drawChart_2(data2){
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom);
+    var option;
 
+    // prettier-ignore
+    let dataAxis = data2['stationName'];
+    // prettier-ignore
+    let data = data2['result'];
+    let yMax = 500;
+    let dataShadow = [];
+    for (let i = 0; i < data.length; i++) {
+        dataShadow.push(yMax);
+    }
+    option = {
+        title: {
+            text: '特性示例：渐变色 阴影 点击缩放',
+            subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
+        },
+        xAxis: {
+            data: dataAxis,
+            axisLabel: {
+                inside: true,
+                color: '#000'
+            },
+            axisTick: {
+                show: false
+            },
+            axisLine: {
+                show: false
+            },
+                z: 10
+        },
+        yAxis: {
+            axisLine: {
+                show: false
+            },
+            axisTick: {
+                show: false
+            },
+            axisLabel: {
+                color: '#999'
+            }
+        },
+        dataZoom: [
+            {
+                type: 'inside'
+            }
+        ],
+        series: [
+            {
+                type: 'bar',
+                showBackground: true,
+                itemStyle: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                        { offset: 0, color: '#83bff6' },
+                        { offset: 0.5, color: '#188df0' },
+                        { offset: 1, color: '#188df0' }
+                    ])
+                },
+                emphasis: {
+                    itemStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                            { offset: 0, color: '#2378f7' },
+                            { offset: 0.7, color: '#2378f7' },
+                            { offset: 1, color: '#83bff6' }
+                        ])
+                     }
+                },
+                data: data
+            }
+        ]
+    };
+    // Enable data zoom when user click bar.
+    const zoomSize = 6;
+    myChart.on('click', function (params) {
+        console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
+        myChart.dispatchAction({
+            type: 'dataZoom',
+            startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
+            endValue:
+                dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
+        });
+    });
+
+    option && myChart.setOption(option);
+
+}
+
+//繪製一般柱狀圖
+function drawChart_1(data){
+    let myChart = echarts.init(mainEl);
+
+    // 指定图表的配置项和数据
+    let option = {
+        title: {
+            text: "PM2.5 數據圖",
+        },
+        tooltip: {},
+        legend: {
+            data: ["站點"],
+        },
+        xAxis: {
+            data:data["stationName"],
+        },
+        yAxis: {},
+        series: [
+            {
+                name: "PM2.5",
+                type: "bar",
+                data: data["result"],
+            }
+        ]
+    };
+
+        // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option); 
+}
 
